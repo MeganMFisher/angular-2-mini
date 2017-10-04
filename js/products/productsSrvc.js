de7dbb4220f4aad2c2423f6ad3907b7dd11d1217
meganmfisher <meganmichellefisher@gmail.com>
2017-10-04 10:52:30 -0600

    completed

--- js/products/productsSrvc.js
+++ js/products/productsSrvc.js
@@ -1,3 +1,13 @@
-angular.module('myApp').service('productSrvc', function() {
+angular.module('myApp').service('productsSrvc', function($http) {
+
+    this.getShoeData = () => {
+        return $http.get('https://practiceapi.devmountain.com/products?category=shoes')
+    
+    }
+
+    this.getSockData = () => {
+        return $http.get('https://practiceapi.devmountain.com/products?category=socks')
+    }
+
 
 });
